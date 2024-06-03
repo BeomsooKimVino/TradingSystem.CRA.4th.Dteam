6ab4c20f70643d70ebff727c517c6a306991568a
kwonluck <software_k@naver.com>
2024-06-03 16:31:29 +0900

    [FEAT] buyNiceTiming 기본 구현
    기본 구조 구현

--- 20240603_TradingSystem/AutoTradingSystem.cpp
+++ 20240603_TradingSystem/AutoTradingSystem.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
+#include "IBroker.h"
 
 using namespace std;
 
 class AutoTradingSystem {
 public:
 	void selectStockBrocker(string type) {
+		if (type == "Kiwer")
+			broker = new KiwerBroker();
+		else if (type == "Nemo")
+			broker = new NemoBroker();
 	}
 
 	void login() {
@@ -24,11 +29,34 @@ public:
 
 	}
 
-	void buyNiceTiming() {
+	void buyNiceTiming(string stockCode, int price) {
+		int prevPrice = 0;
+		int climbCount = 0;
 
+		for (int checkCnt = 0; checkCnt < MAX_BUY_CECHK_COUNT; checkCnt++)
+		{
+			int currentPrice = broker->currentPrice(stockCode);
+			if (prevPrice < currentPrice) {
+				climbCount = 0;
+				continue;
+			}
+
+			climbCount++;
+			if (climbCount > MAX_BUY_CLIMB_COUNT)
+			{
+				broker->buy(stockCode, price / currentPrice, currentPrice);
+			}
+			prevPrice = currentPrice;
+		}
 	}
 
-	void sellNiceTiming() {
-
+	void sellNiceTiming() {	
+		
 	}
+
+private:
+	IBroker* broker;
+
+	const int MAX_BUY_CECHK_COUNT = 24;
+	const int MAX_BUY_CLIMB_COUNT = 3;
 };
